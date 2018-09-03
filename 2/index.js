87c45cf08cda12189a2c22fb8fea106e558372fe
Anton Batalin <antbat2009@gmail.com>
2018-09-03 08:24:33 +0300

    numbers and strings

--- 2/index.js
+++ 2/index.js
@@ -1,9 +1,30 @@
 console.log('start');
 
+// boolean
 const a = true;
 const b = false;
-const c = a + b;
+
+const c = a || b;
+const d = a && b;
 
 console.log(c);
+
+// number
+
+const x = 1;
+const y = 0.5;
+
+const z = x + y;
+console.log(z);
+
+// string
+const first = 'first string';
+const second = 'second string';
+
+const result = first + second;
+console.log(result)
+
+
+
 
 console.log('finish');
