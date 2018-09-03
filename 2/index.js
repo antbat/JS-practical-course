8235b6a7460aad194d48509c1720af8fc2f74c6d
Anton Batalin <antbat2009@gmail.com>
2018-09-03 08:56:25 +0300

    objects and arrays

--- 2/index.js
+++ 2/index.js
@@ -24,7 +24,55 @@ const second = 'second string';
 const result = first + second;
 console.log(result)
 
+// boolean again
+
+console.log(`${x} < ${y} = ${x < y}`);
 
 
+// array
+const weekDay = ['monday', 'tuesday', 'wednesday']
+console.log(weekDay[0]);
+console.log(weekDay[2]);
+console.log(weekDay[1]);
+
+// objects
+
+const rectangle = {
+    x: 20,
+    x: 10,
+    width: 100,
+    heigh: 200
+}
+
+const men = {
+    firstName: 'Alex',
+    lastName: 'Bondarenko',
+    age: 2,
+    location: 'Kiev',
+    lang: 'own'
+}
+
+const offer = {
+    price: 0,
+    lang: 'ru',
+    title: 'Мое предложение',
+    description: 'это замечательное предложение цены на русском языке',
+    discountDays: ['понедельник', 'пятница'],
+    schedule: [
+        {
+            weekDay: 'monday',
+            start: '8:00',
+            finish: '19:00'
+        }
+    ]
+}
+
+
+// null, undefined; variables
+
+let one;
+console.log(one);
+console.log(x + one);
+const m = 'qwerty'.match(/[aeiou]/gi);
 
 console.log('finish');
