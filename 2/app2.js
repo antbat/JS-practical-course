b72697a407683c260ece68e1198eb2328228f2af
TT\Tatiana <taloza@gmail.com>
2018-09-07 11:29:29 +0300

    first trying to use classes in top10 and in training

--- 2/app2.js
+++ 2/app2.js
@@ -1,8 +1,10 @@
 console.log('--- start  ---');
 
-const arrSize = 1e7;  //1e3 – это 1 с 3 нулями, то есть 1000.
+const arrSize = 100;  //1e3 – это 1 с 3 нулями, то есть 1000.
 const min = 0;
-const max = 1e4;
+const max = 100;
+
+let timeStart = Date.now();
 
 function generateHugeArray(arrSize, min, max){
     let arr = [];
@@ -19,50 +21,58 @@ function generateHugeArray(arrSize, min, max){
 let hugeArray = generateHugeArray(arrSize, min, max);
 // console.log('hugeArray:' + (hugeArray.length)+ ': ' + hugeArray);
 
+let timeEnd = Date.now();
+console.log(`time delay is: ${timeEnd-timeStart} milliseconds`);
 
 const {performance} = require('perf_hooks');
 var t0 = performance.now();
 
 
-// find max values from 1M huge array
-function getMaxVal(hugeArray){
-    let maxVal = hugeArray[0]; 
-        for (let i = 0; i < hugeArray.length; i++) { 
-            if (maxVal < hugeArray[i]){
-                maxVal = hugeArray[i]; 
-            }
-        }
-    return maxVal
-};
+class NewTop10{  
 
-const maxVal = getMaxVal(hugeArray);
+    constructor(hugeArray){
+        this.hugeArray = hugeArray;
 
-
-function getTop10(hugeArray, maxVal){
-    let top10 = [];
-    var value;
-    try{
-        for (let n = maxVal0; n > 0  &&  top10.length < 10; n--){ 
-           // if (top10.length == 10) break;
-            
-            for (let i = 0; i < hugeArray.length && top10.length < 10; i++)        
-             {      
-                value = hugeArray[i];           
-                if (n <= value && (value <= n) ){               
-                    top10.push(value)
-                }  
-            }       
-        } 
     }
-    catch(err){         
-        console.log(err.name + 'something goes wrong')
-    }    
-    return top10
-}
+// find max values from 1M huge array
+    getMaxVal(){
+        let maxVal = this.hugeArray[0]; 
+            for (let i = 0; i < this.hugeArray.length; i++) { 
+                if (maxVal < this.hugeArray[i]){
+                    maxVal = this.hugeArray[i]; 
+                }
+            }
+        return maxVal
+    };
 
+    getTop10(){
+        const maxVal = this.getMaxVal(this.hugeArray);
+        let top10 = [];
+        var value;
+        try{
+            for (let n = maxVal; n > 0  &&  top10.length < 10; n--){ 
+                // if (top10.length == 10) break;            
+                for (let i = 0; i < this.hugeArray.length && top10.length < 10; i++){      
+                    value = this.hugeArray[i];           
+                    if (n <= value && (value <= n) ){               
+                        top10.push(value)
+                    }  
+                }       
+            } 
+        }
+        catch(err){         
+            console.log(err.name + 'something goes wrong')
+        }    
+        return top10
+    }
+}
    
-let top10 = getTop10(hugeArray, maxVal);
-console.log('Top10:'  + (top10.length) + ': ' +  top10);
+// let top10 = getTop10(hugeArray, maxVal);
+let newTop10 = new NewTop10(hugeArray);
+
+let res = newTop10.getTop10();
+
+console.log('Top10:'  + (res.length) + ': ' +  res);
 
 
 var t1 = performance.now();
