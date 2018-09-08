a98e29db78e2c5abbeb41c2f24ca8e49c89c0eb3
Anton Batalin <antbat2009@gmail.com>
2018-09-08 14:51:07 +0300

    add timer with external output result

--- pilot/app.js
+++ pilot/app.js
@@ -1,10 +1,18 @@
 
 // declaration
+let timer;
 function generate(size){
     return []
 }
-function start(){}
-function finish(){ return 1;}
+function start(){
+    const now = new Date();
+    timer = now.getTime();
+}
+function finish(){
+    const now = new Date();
+    const delay = now.getTime() - timer;
+    return delay;
+}
 function top(size){
     return []
 }
@@ -15,4 +23,4 @@ const data = generate(100);
 start();
 top(10);
 const period = finish();
-console.log('app has finished up');
+console.log(`app has finished up ${period / 1000} sec`);
