a26ae77890d14999f0dd3078a670c9c75bad591c
Anton Batalin <antbat2009@gmail.com>
2018-09-08 14:45:16 +0300

    main scenario

--- pilot/app.js
+++ pilot/app.js
@@ -1,2 +1,18 @@
+
+// declaration
+function generate(size){
+    return []
+}
+function start(){}
+function finish(){ return 1;}
+function top(size){
+    return []
+}
+
+// here we start use declared function
 console.log('start');
+const data = generate(100);
+start();
+top(10);
+const period = finish();
 console.log('app has finished up');
