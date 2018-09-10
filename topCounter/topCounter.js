3f97a33fc34b3bd3c856a73b652e324a7ce756a0
Anton Batalin <antbat2009@gmail.com>
2018-09-10 09:41:45 +0300

    added profiler

--- topCounter/topCounter.js
+++ topCounter/topCounter.js
@@ -1,8 +1,18 @@
 
+let timer;
+
 // declare function area
 function generate(){}
-function start(){}
-function finish(){}
+function start(){
+    const now = new Date();
+    timer = now.getTime();
+}
+function finish(){
+    const now = new Date();
+    const delay = now.getTime() - timer;
+    console.log('sec: ', delay / 1000);
+    return delay;
+}
 function countTop(){}
 
 
