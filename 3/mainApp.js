747019a92d23fbf86e76c2e3715817becb77e241
TT\Tatiana <taloza@gmail.com>
2018-09-10 13:02:45 +0300

    adding main.App to new main branch

---
+++ 3/mainApp.js
@@ -0,0 +1,10 @@
+// Will have 4 steps:
+// 1. create timer
+// 2. create random huge array
+// 3. find top N=10 from different numbers
+// 4. merge all branches to main
+
+class Timer{}
+class HugeArrayRandom{}
+class GetTop{}
+
