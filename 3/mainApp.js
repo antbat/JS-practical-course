149c6f4fba7b2fe9c2f2878bf7c6aea691a6948d
TT\Tatiana <taloza@gmail.com>
2018-09-10 13:07:50 +0300

    adding getTopNew

--- 3/mainApp.js
+++ 3/mainApp.js
@@ -8,3 +8,23 @@ class Timer{}
 class HugeArrayRandom{}
 class GetTop{}
 
+getTop10(){
+    const maxVal = this.getMaxVal(this.hugeArray);
+    let top10 = [];
+    var value;
+    try{
+        for (let n = maxVal; n > 0  &&  top10.length < 10; n--){ 
+            // if (top10.length == 10) break;            
+            for (let i = 0; i < this.hugeArray.length && top10.length < 10; i++){      
+                value = this.hugeArray[i];           
+                if (n <= value && (value <= n) ){               
+                    top10.push(value)
+                }  
+            }       
+        } 
+    }
+    catch(err){         
+        console.log(err.name + 'something goes wrong')
+    }    
+    return top10
+}
