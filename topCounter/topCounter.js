af267f4c18c0248e7a8eb1031bdadc1c0e736950
Anton Batalin <antbat2009@gmail.com>
2018-09-10 09:30:17 +0300

    main steps of app

--- topCounter/topCounter.js
+++ topCounter/topCounter.js
@@ -1,2 +1,15 @@
+
+// declare function area
+function generate(){}
+function start(){}
+function finish(){}
+function countTop(){}
+
+
+// apply
 console.log('hi!');
+const data = generate();
+start();
+const top = countTop();
+finish();
 
