a1558f864db0e2d527f118ef46f5b78de5ddcffc
TT\Tatiana <taloza@gmail.com>
2018-09-10 13:05:05 +0300

    add class HugeArrayRandom

--- 3/mainApp.js
+++ 3/mainApp.js
@@ -8,3 +8,24 @@ class Timer{}
 class HugeArrayRandom{}
 class GetTop{}
 
+class HugeArrayRandom{ //to create random huge array
+    constructor(arrSize, min, max){
+        this.arrSize = arrSize;
+        this.min = min;
+        this.max = max;
+        // this.randomInteger = randomInteger(); - this will make ReferenceError
+    }
+
+    generateHugeArray(){
+        let arr = [];
+        for (let i = 0; i < this.arrSize; i++ ){       
+            arr.push(this._randomInteger());
+        }
+        return arr;    
+    };    
+
+    _randomInteger() {
+        var rand = Math.round(this.min + Math.random() * (this.max - this.min));         
+        return Math.round(rand);
+    } 
+}
