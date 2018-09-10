c0f5975374be0e895341d182a2fe267739439e35
Anton Batalin <antbat2009@gmail.com>
2018-09-10 09:36:16 +0300

    added generator funtion

--- topCounter/topCounter.js
+++ topCounter/topCounter.js
@@ -1,6 +1,12 @@
 
 // declare function area
-function generate(){}
+function generate(size = 1000, min = 1, max = 100){
+    const hugeData = [];
+    for(let i=0; i < size; i++){
+        hugeData.push(Math.random()*max + min);
+    }
+    return hugeData;
+}
 function start(){}
 function finish(){}
 function countTop(){}
