7b6366bc65b5144ab9f153ccb910cce03a8a7506
Anton Batalin <antbat2009@gmail.com>
2018-09-10 09:51:19 +0300

    added top counter engine

--- topCounter/topCounter.js
+++ topCounter/topCounter.js
@@ -1,15 +1,27 @@
 
 // declare function area
-function generate(){}
+function generate(){
+    return [];
+}
 function start(){}
 function finish(){}
-function countTop(){}
+function countTop(data, size){
+    const top = new Array(size);
+    const lastElementIndex = size - 1;
+    data.forEach( e => {
+        if(e > top[lastElementIndex]) {
+            top[lastElementIndex] = e;
+            top.sort((a, b) => b - a);
+        }
+    })
+    return top;
+}
 
 
 // apply
 console.log('hi!');
 const data = generate();
 start();
-const top = countTop();
+const top = countTop(data);
 finish();
 
