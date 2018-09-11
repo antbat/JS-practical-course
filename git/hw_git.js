da8f3cff5be59f06e7fd96d01a7cc002c660cca4
noachase <noach4se@gmail.com>
2018-09-11 03:37:57 +0200

    Added Timer

--- git/hw_git.js
+++ git/hw_git.js
@@ -19,5 +19,40 @@ class ArrayMaker {
 }
 
 
-const arrayMaker = new ArrayMaker (1000);
-console.log(array);
+class Timer {
+    constructor(isSec){
+        this.isSec = isSec;
+        console.log(`Created as "${this.isSec ? 'sec' : 'ms'}" timer`);
+    }
+    start(){
+        const now = new Date();
+        this.timer = now.getTime();
+        console.log(`Started at ${now.getHours()}: ${now.getMinutes()}`);
+    }
+    finish() {
+        const now = new Date();
+        const timeDiff = now.getTime() - this.timer;
+        console.log(`finished at ${ this.isSec ?  timeDiff / 1000 : timeDiff } sec`);
+    }
+
+
+}
+
+const timerSec = new Timer(true);
+const timerMs = new Timer(false);
+const arrayMaker = new ArrayMaker (1000000);
+// console.log(arrayMaker);
+const array1 = arrayMaker.createArray();
+// console.log(array1);
+
+try {
+    timerSec.start();
+    timerMs.start();
+    
+    timerSec.finish();
+    timerMs.finish();
+
+    console.log(`Array ${array1.length} was created`);
+} catch(err){
+    console.error(err);
+}
