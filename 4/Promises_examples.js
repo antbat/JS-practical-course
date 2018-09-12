ee421d7b2b85d52b71bf74a591fdb84cc91e001e
noachase <noach4se@gmail.com>
2018-09-13 00:09:54 +0200

    promises_example_done

--- 4/Promises_examples.js
+++ 4/Promises_examples.js
@@ -8,25 +8,41 @@ function applyForJob(docs) {
     return promise;
 }
 
-function celebrate(){
+
+function approveApply(permission){
+    console.log('WE GOT APPROVED');
+    return permission;
+}
+// approved => {
+//     console.info('Ur apply has been approved');
+//     return approved;
+// };
+
+function celebrate(permission){
+    console.log(permission);
     console.log('------------------------');
     console.log('EAT=>SLEEP=>RAVE=>REPEAT');
+    return new Promise(function(resolve, reject){
+        setTimeout(() => resolve(permission), 1500);
+    });
 }
 
 function getReady(){
     console.log('------------------------');
     console.log('Get pants and shirt steamed etc.');
+    return console.log('We are ready to work,' + {});
 }
 
 applyForJob({})
-    .then(approved => console.info('Ur apply has been approved'),
-        reason => console.error(reason))
+    .then(approveApply)
     .then(celebrate)
     .then(getReady)
     .catch(error => console.error(error));
-        // function(approved){
-    //     console.info('Ur apply is approved!');
-    // },
-    // function(reason){
-    //     console.error(reason);
-    // });
+
+
+//     function(approved){
+//     console.info('Ur apply is approved!');
+// },
+// function(reason){
+//     console.error(reason);
+// });
