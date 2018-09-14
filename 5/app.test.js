b371494cd205278f2fa4e086a690f302368cf629
Anton Batalin <antbat2009@gmail.com>
2018-09-14 09:08:52 +0300

    minor bug fix

--- 5/app.test.js
+++ 5/app.test.js
@@ -7,11 +7,12 @@ describe('async flow', async () => {
     it('should provide data "N" times', async () => {
         const expectedCount = 5;
         let counter = 0;
-        function incrementCounter(data){
+        function incrementCounter(smth){
+            console.log(smth);
             counter++;
         }
-        const randomNumericAsyncStream = new RandomNumericAsyncStream(incrementCounter, 5, 0, 1000);
+        const randomNumericAsyncStream = new RandomNumericAsyncStream(incrementCounter, 5, 0, expectedCount);
         await randomNumericAsyncStream.start();
-        counter.should.be.eql(5);
+        counter.should.be.eql(expectedCount);
     });
 });
