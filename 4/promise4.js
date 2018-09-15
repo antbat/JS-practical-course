9ee32706feb28a499d376d3c23d17b387cfd8171
TT\Tatiana <taloza@gmail.com>
2018-09-15 23:08:01 +0300

    sucsess simple promise

--- 4/promise4.js
+++ 4/promise4.js
@@ -5,7 +5,7 @@ function isExamPassed(resolve, reject){
     const random = Math.random();    
     console.log(random);
     
-    if( random >= 0.1){       
+    if( random >= 0.9){       
             console.log('yes, ExamPassed !!!')           
             resolve(123);
         }    
@@ -17,12 +17,12 @@ let promise1 = new Promise(isExamPassed);
 console.log(promise1)
 
 function nextStep1(ExamPassed){   
-    console.log(ExamPassed)
-    // return (ExamPassed)
+    console.log(ExamPassed + ' nextStep1')
+     return (ExamPassed)
 };
 
 function nextStep2(ExamPassed){   
-    console.log(ExamPassed)   
+    console.log(ExamPassed + ' nextStep2')   
 };
 
 promise1
