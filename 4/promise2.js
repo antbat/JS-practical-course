f436d63d6b0ce271e6ea9a31f516afdcb07d1139
TT\Tatiana <taloza@gmail.com>
2018-09-17 10:58:05 +0300

    async await trying

--- 4/promise2.js
+++ 4/promise2.js
@@ -59,3 +59,34 @@ console.log ('___finish___');
 
 
 
+console.log('применяйте PROMISE > 2h');
+
+
+function isExamPassed(resolve, reject){  
+    const random = Math.random();    
+    console.log(random);
+    
+    if( random >= 0.9){       
+            console.log('yes, ExamPassed !!!')           
+            resolve(123);
+        }    
+    else reject('error!!! error!!!');
+ 
+};
+
+let promise1 = new Promise(isExamPassed);
+console.log(promise1)
+
+function nextStep1(ExamPassed){   
+    console.log(ExamPassed + ' nextStep1')
+     return (ExamPassed)
+};
+
+function nextStep2(ExamPassed){   
+    console.log(ExamPassed + ' nextStep2')   
+};
+
+promise1
+.then(nextStep1)
+.then(nextStep2)
+.catch(error => console.error(error));
--- 4/promise4.js
+++ 4/promise4.js
@@ -4,13 +4,14 @@ console.log('применяйте PROMISE > 2h');
 function isExamPassed(resolve, reject){  
     const random = Math.random();    
     console.log(random);
+    setTimeout(function(){
+        if( random >= 0.2){       
+                console.log('yes, ExamPassed !!!');           
+                resolve(12);
+            }    
+        else reject('error!!!');
+    }, 5000)
     
-    if( random >= 0.9){       
-            console.log('yes, ExamPassed !!!');           
-            resolve(12);
-        }    
-    else reject('error!!!');
- 
 };
 
 let promise1 = new Promise(isExamPassed);
@@ -18,8 +19,7 @@ console.log(promise1)
 
 function nextStep1(ExamPassed){   
     console.log(ExamPassed  + ' step1');
-    return new Promise(nextStep2);
-        
+    return new Promise(nextStep2);        
     // return (ExamPassed)
 };
 
@@ -35,15 +35,17 @@ function nextStep2(resolve, reject){
         console.log(' step2 sucsess')          
             resolve(123);
         }    
-    else reject('error!!! error!!! + step2');
-  
+    else reject('error!!! error!!! + step2'); 
     
 };
 
 promise1
 .then(nextStep1)
-.catch(error => console.error(error));
-
-nextStep1()
 .then(nextStep3)
-.catch(error => console.error(error  + 'step3'));
+.catch(error => 
+    console.error(error)
+    );
+
+// nextStep1()
+// .then(nextStep3)
+// .catch(error => console.error(error  + 'step3'));
