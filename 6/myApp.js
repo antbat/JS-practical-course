00ec8daa1565b6a0090845062757b3c8520d6fd6
TT\Tatiana <taloza@gmail.com>
2018-09-20 12:47:17 +0300

    adding server

---
+++ 6/myApp.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const myApp = express();
+
+
+console.log('-------start------');
+
+function content(req, res){
+    console.log(__dirname);
+    res.send('<h1>hello!!!</h1>');
+    
+}
+myApp.get('/', content); //run app
+
+myApp.set('siteTitle', 'My Site');
+console.log(myApp.get('siteTitle')); // "My Site"
+
+myApp.get('/1', function(req, res) {
+    res.send('page1');
+});
+
+
+
+myApp.use(express.static(__dirname +'/public')); //didnt work
+
+
+myApp.listen(3000);
+console.log('-------finish------');
+
+
+
