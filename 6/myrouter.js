41e7809868442a35f52632f76e6bfb33d6e05518
Evgen Markelov <noach4se@gmail.com>
2018-09-21 08:58:54 +0200

    added router as module "myrouter.js"

--- 6/myrouter.js
+++ 6/myrouter.js
@@ -5,4 +5,4 @@ function myRouter(req, res) {
     const now = new Date();
     res.send(`${counter++}) I am working here !! now = ${now} data = ${data}`);
 }
-exports.myRouter = myRouter;
+module.exports.myRouter = myRouter;
