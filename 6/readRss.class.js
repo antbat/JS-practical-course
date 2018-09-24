43bcc768542de1e1b0638129a2ada2c884c45abb
Anton Batalin <antbat2009@gmail.com>
2018-09-24 09:35:39 +0300

    refactoring

--- 6/readRss.class.js
+++ 6/readRss.class.js
@@ -4,23 +4,24 @@ class ReadRss {
     constructor(settings){
         this.url = settings && settings.url || '';
     }
-    async getData(){
-        // #1 get data
+    async dataProcess(){
 
+        // #1 get data
         const xml = await this._getDataFromExternalSource();
 
-        // #1 parsing
+        // #2 parsing
+        const json = await this._parseToJson(xml);
 
-        // parseString(xml, function (err, result) {
-        //     console.dir(result);
-        // });
-        // const data = await this._parce(xml)
+        // #3 split into separate articles
+        const articles = this._separateToArticles(json);
 
-        // #2 split into separate articles
-        //
-
-        // #3 convert article into internal format
-        //
+        // save to DB
+        if(articles && articles.length > 0){
+            const max =  articles.length;
+            for(let i=0; i < max; i++) {
+                await this._saveArticlesToMonoDB(articles[i]);
+            }
+        }
 
     }
     async _getDataFromExternalSource(){
@@ -38,6 +39,30 @@ class ReadRss {
                 }
             });
         });
+    }
+
+    async _parseToJson(data){
+        return new Promise( (resolve, reject) => {
+            parseString(data, function (err, result) {
+                console.dir(result);
+                if(err){
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+
+            });
+        });
+
+    }
+
+    _separateToArticles(data){
+        return [];
+    }
+    async _saveArticlesToMonoDB(article){
+        // mongoosjs
+        // https://mongoosejs.com
+        return new Promise();
     }
 }
 
