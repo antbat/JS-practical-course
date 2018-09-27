e84bd78f2260e20272a6402e0cc5029b99d9eed3
Anton Batalin <antbat2009@gmail.com>
2018-09-27 10:46:46 +0300

    add configuration

--- 6/app.js
+++ 6/app.js
@@ -1,12 +1,22 @@
+process.env.NODE_CONFIG_DIR = __dirname + '/config/';
+
 const express = require('express');
 const ReadRss = require('./service/readRss.service.js');
+
+
+const config = require('config');
+const port = process.env.PORT || config.get('port');
+const mongodb = config.get('mongodb');
 
 const RBK = require('./RSS/RBK/rbk.settings');
 const Korrespondent = require('./RSS/Korrespondent/Korrespondenr.settings');
 const Facts = require('./RSS/Facts/facts.settings');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rss');
+mongoose.connect(mongodb.connection);
+
+
+const Article = require('./model/article.model');
 
 
 const app = express();
@@ -14,18 +24,21 @@ let counter = 1;
 
 console.log('start here');
 
-function myRouter(req, res) {
+async function myRouter(req, res) {
     const data = req.query.p;
+
+    const allArticles = await Article.find({});
 
     console.log('I get request from web browser');
     const now = new Date();
     // get all articles from DB
     // send ones to browser
-    res.send(`${counter++}) I am working here !! now = ${now} data = ${data}`);
+    // res.send(`${counter++}) I am working here !! now = ${now} data = ${data}`);
+    res.send(allArticles);
 }
 
 app.get('/', myRouter);
-app.listen(3010);
+app.listen(port);
 
 
 setTimeout( async () =>{
