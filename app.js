039b70903c7a584c8c875629b5176a662a6be5a3
TT\Tatiana <taloza@gmail.com>
2018-09-30 23:13:48 +0300

    add new rss files

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const mongodb = config.get('mongodb');
 const RBK = require('./RSS/RBK/rbk.settings');
 const Korrespondent = require('./RSS/Korrespondent/Korrespondenr.settings');
 const Facts = require('./RSS/Facts/facts.settings');
+const Finance = require('./RSS/Finance/finance.settings');
+const Tyzhden = require('./RSS/Tyzhden/tyzhden.settings');
 
 const mongoose = require('mongoose');
 mongoose.connect(mongodb.connection);
@@ -20,17 +22,17 @@ const Article = require('./model/article.model');
 
 
 const app = express();
-let counter = 1;
+// let counter = 1;
 
 console.log('start here');
 
 async function myRouter(req, res) {
-    const data = req.query.p;
+    // const data = req.query.p;
 
     const allArticles = await Article.find({});
 
     console.log('I get request from web browser');
-    const now = new Date();
+    // const now = new Date();
     // get all articles from DB
     // send ones to browser
     // res.send(`${counter++}) I am working here !! now = ${now} data = ${data}`);
@@ -40,25 +42,36 @@ async function myRouter(req, res) {
 app.get('/', myRouter);
 app.listen(port);
 
+setTimeout( async () =>{
+    // URLS error
+    const rssReaderTyzhden = new ReadRss(Tyzhden);
+    await rssReaderTyzhden.dataProcess();
+}, 10);
 
 setTimeout( async () =>{
     // URLS error
-    const rssReaderRBK = new ReadRss(RBK);
-    await rssReaderRBK.dataProcess();
-}, 1000);
+    const rssReaderFinance = new ReadRss(Finance);
+    await rssReaderFinance.dataProcess();
+}, 100);
 
-setTimeout( async () =>{
-    // init error
-    // const rssReaderFacts = new ReadRss();
-    const rssReaderFacts = new ReadRss(Facts);
-    await rssReaderFacts.dataProcess();
-}, 10000);
+// setTimeout( async () =>{
+//     // URLS error
+//     const rssReaderRBK = new ReadRss(RBK);
+//     await rssReaderRBK.dataProcess();
+// }, 1000);
 
-setTimeout( async () =>{
-    // error free
-    const rssReaderKorrespondent = new ReadRss(Korrespondent);
-    await rssReaderKorrespondent.dataProcess();
-}, 5000);
+// setTimeout( async () =>{
+//     // init error
+//     // const rssReaderFacts = new ReadRss();
+//     const rssReaderFacts = new ReadRss(Facts);
+//     await rssReaderFacts.dataProcess();
+// }, 2000);
+
+// setTimeout( async () =>{
+//     // error free
+//     const rssReaderKorrespondent = new ReadRss(Korrespondent);
+//     await rssReaderKorrespondent.dataProcess();
+// }, 3000);
 
 
 
